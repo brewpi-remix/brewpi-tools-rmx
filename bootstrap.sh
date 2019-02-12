a314a2b80137d8ed0c327695a09ccac154a9de21
Lee Bussy <Lee@Bussy.org>
2019-02-11 19:19:16 -0600

    Attempt at not enforcing pi user

--- bootstrap.sh
+++ bootstrap.sh
@@ -92,10 +92,8 @@ func_comline() {
 
 func_checkroot() {
   if [ "$SUDO_USER" ]; then REALUSER=$SUDO_USER; else REALUSER=$(whoami); fi
-  if [[ $EUID -ne 0 ]]; then UIDERROR="root";
-  elif [[ $REALUSER != "pi" ]]; then UIDERROR="pi"; fi
-  if [[ ! $UIDERROR == ""  ]]; then
-    echo -e "This script must be run by user 'pi' with sudo."
+  if [[ $EUID -ne 0 ]]; then
+    echo -e "This script must be run with root priviledges."
     echo -e "Enter the following command as one line:"
     echo -e "wget -q $GITRAW -O - /| sudo bash\n" 1>&2
     exit 1
@@ -329,8 +327,8 @@ func_clonetools() {
     echo -e "'N' below, and add a new chamber by executing:"
     echo -e "'sudo $homepath/$GITPROJ/install.sh'\n"
     read -p "Remove $homepath/$GITPROJ? [y/N] " < /dev/tty
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-      rm -fr $homepath/$GITPROJ
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+      rm -fr "$homepath/$GITPROJ"
       echo
     else
       echo -e "\nLeaving $homepath/$GITPROJ in place and exiting.\n"
@@ -338,8 +336,8 @@ func_clonetools() {
     fi
   fi
 
-  gitClone="git clone $GITCMD $homepath/$GITPROJ"
-  eval $gitClone||die
+  gitClone="sudo -u $REALUSER git clone $GITCMD $homepath/$GITPROJ"
+  eval "$gitClone"||die
 }
 
 ############
