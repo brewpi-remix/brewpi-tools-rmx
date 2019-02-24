04977afa2ec881b6390224baab58d7bfa498b3ea
LBussy <lee@bussy.org>
2019-02-24 12:25:21 -0600

    Updated checkroot() for relaunch

--- bootstrap.sh
+++ bootstrap.sh
@@ -88,16 +88,25 @@ func_comline() {
 }
 
 ############
-### Make sure user pi is running with sudo
+### Make sure command is running with sudo
 ############
 
 func_checkroot() {
   if [ "$SUDO_USER" ]; then REALUSER=$SUDO_USER; else REALUSER=$(whoami); fi
   if [[ $EUID -ne 0 ]]; then
-    echo -e "\nThis script must be run with root priviledges."
-    echo -e "Enter the following command as one line:"
-    echo -e "$CMDLINE" 1>&2
-    exit 1
+    sudo -n true 2> /dev/null
+    if [[ ${?} == "0" ]]; then
+      echo -e "\nNot runing as root, relaunching correctly.\n"
+      sleep 2
+      exec "$CMDLINE"
+      exit $?
+    else
+      # sudo not available, give instructions
+      echo -e "\nThis script must be run with root priviledges."
+      echo -e "Enter the following command as one line:"
+      echo -e "$CMDLINE" 1>&2
+      exit 1
+    fi
   fi
   # And get the user home directory
   _shadow="$( (getent passwd "$REALUSER") 2>&1)"
